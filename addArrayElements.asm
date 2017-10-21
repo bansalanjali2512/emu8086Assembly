570ee9687bd2bdca8d862fab7843f3b1eeb1c72b
DeltaSquareDUCS <anjali.mca17.du@gmail.com>
2017-10-21 23:31:35 +0530

    Assembly program to add elements of array

---
+++ addArrayElements.asm
@@ -0,0 +1,24 @@
+Data segment
+    sum dw 0
+    index dw 0
+    array dw 1,2,3,4,5
+Data ends
+
+code segment
+    assume DS: Data
+    start:
+        MOV AX,Data
+        MOV DS,AX
+        MOV SI,0
+        MOV CX,4
+        XOR AX,AX 
+        LEA BX,array
+        ADD AX,BX[SI]       
+    next:
+        INC SI  
+        INC SI
+        ADC AX,BX[SI]
+        LOOP next    
+    MOV sum,AX
+code ends
+end start
